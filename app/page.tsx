68b633f6edfe98763d5b6ac20e49f164de280ef0
Dennis Blömeke <de.bloemeke@gmail.com>
2024-08-28 15:45:44 +0200

    Initial commit from Create Next App

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import Hero from "@/components/hero";
+import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
+import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
+import { verifySupabase } from "@/utils/supabase/verify-supabase";
+
+export default async function Index() {
+  return (
+    <>
+      <Hero />
+      <main className="flex-1 flex flex-col gap-6 px-4">
+        <h2 className="font-medium text-xl mb-4">Next steps</h2>
+        {verifySupabase ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
+      </main>
+    </>
+  );
+}
